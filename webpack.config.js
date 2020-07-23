eadc57548a3dfd5750064d83887fbfac09846c00
Evgeniy Panin <pane.ewgen@yandex.ru>
2020-07-23 14:00:05 +0400

    Перед настройкой сборки JS

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,18 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 const isDev = process.env.NODE_ENV === 'development';
+
 
 const path = require('path');
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: {
+        main: './src/index.js',
+/*         about: './src/about/index.js',
+        analytics: './src/analytics/index.js' */
+ },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[chunkhash].js'
@@ -50,7 +56,7 @@ module.exports = {
         },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'style.[contenthash].css'
+            filename: '[name].[contenthash].css'
         }),
         new OptimizeCssAssetsPlugin({
             assetNameRegExp: /\.css$/g,
@@ -61,8 +67,19 @@ module.exports = {
             template: './src/index.html',
             filename: 'index.html'
         }),
+        new HtmlWebpackPlugin({ 
+            inject: false,
+            template: './src/about/about.html',
+            filename: 'about.html'
+        }),
+        new HtmlWebpackPlugin({ 
+            inject: false,
+            template: './src/analytics/analytics.html',
+            filename: 'analytics.html'
+        }),
         new webpack.DefinePlugin({
             'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        })
+        }),
+        new WebpackMd5Hash()
     ]
 }
