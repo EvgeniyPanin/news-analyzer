c819623dee7b3e7f6815d597ea40ba621e276759
Evgeniy Panin <pane.ewgen@yandex.ru>
2020-07-24 13:29:19 +0400

    добавил editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     module: {
         rules: [{
                     test: /\.js$/,
-                    use: { loader: 'babel-loader' },
+                    use: {loader: 'babel-loader'},
                     exclude: /node_modules/
                 },
                 {
